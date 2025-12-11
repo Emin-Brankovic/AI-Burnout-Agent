212a012f3f40a1f2ae8721256ee39ad011059a97
Emin Brankovic <emin.brankovic19@gmail.com>
2025-12-11 21:47:14 +0100

    first model version

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import string
+from pydantic import BaseModel
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class BurnoutFeatures(BaseModel):
+    social_interactions: float
+    fatigue_level: float
+    physical_activity_minutes: float
+    stress_level: float
+    sleep_hours: float
+    workload: float
+    anxiety_level: float
+    mood_score: float
+    timestamp:string
